9a4b0ba710226c495b2bf338c7938c1324c51f01
hussainsheriffC <hussain.sheriff@cloudreach.com>
2023-05-08 15:13:21 +0530

    ssh agent 2

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,17 +1,17 @@
-provider "kubernetes" {
-  #load_config_file = "false"
-  host = data.aws_eks_cluster.myapp-cluster.endpoint
-  token = data.aws_eks_cluster_auth.myapp-cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
-}
+# provider "kubernetes" {
+#   #load_config_file = "false"
+#   host = data.aws_eks_cluster.myapp-cluster.endpoint
+#   token = data.aws_eks_cluster_auth.myapp-cluster.token
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
+# }
 
-data "aws_eks_cluster" "myapp-cluster" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster" "myapp-cluster" {
+#   name = module.eks.cluster_name
+# }
 
-data "aws_eks_cluster_auth" "myapp-cluster" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster_auth" "myapp-cluster" {
+#   name = module.eks.cluster_name
+# }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
